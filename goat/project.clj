4400c9a4099263052268633aca37847eb57016e0
Gerrit <gerritjvv@gmail.com>
2013-10-03 15:01:24 +0200

    update description and url

--- goat/project.clj
+++ goat/project.clj
@@ -1,11 +1,10 @@
 (defproject goat "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Measuring and Profiling performance for Clojure Apps from the Repl"
+  :url "https://github.com/gerritjvv/goat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ 
                   [robert/hooke "1.3.0"]
-		  ;[dire "0.4.4"]
 		  [org.clojure/clojure "1.5.1"]]
   
   :global-vars {*warn-on-reflection* true}
